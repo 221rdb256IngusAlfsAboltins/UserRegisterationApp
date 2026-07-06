ede852f7bada7a9aa82214d17ff162e12da85633
AlfsAboltins <ingus-alfs.aboltins@edu.rtu.lv>
2026-07-06 14:41:21 +0300

    Renamed project

--- TaskApp/asgi.py
+++ TaskApp/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for UserRegistration project.
+ASGI config for TaskApp project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UserRegistration.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TaskApp.settings')
 
 application = get_asgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UserRegistration.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TaskApp.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
